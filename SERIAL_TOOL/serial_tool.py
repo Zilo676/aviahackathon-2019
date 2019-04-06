c8482217007b71320d138d90bd34be460ca35fe4
Николаев Богдан Сергеевич <Nikolaev.Bogdan@geropharm.com>
2019-04-06 12:40:42 +0300

    Save

--- SERIAL_TOOL/serial_tool.py
+++ SERIAL_TOOL/serial_tool.py
@@ -4,13 +4,14 @@ from threading import Thread
 
 import struct
 
-sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM);
-sock.bind(('localhost', 5000))
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.bind(('127.0.0.1', 5000))
 sock.listen(1)
 
 conn, addr = sock.accept()
 
 DATA = []
+
 
 def recv_serial(port):
     ser = serial.Serial(port=port, baudrate=115200)
@@ -46,11 +47,12 @@ def recv_serial(port):
         if checksum // 256 >= 1:
             checksum = checksum % 256
         # if checksum == checksumfromb:
-        DATA.append([ struct.pack('d',((MSB[i] & 0x7F) << 7) | (LSB[i] & 0x7F)) for i, M in enumerate(MSB)])
+        DATA.append([struct.pack('d', ((MSB[i] & 0x7F) << 7) | (LSB[i] & 0x7F)) for i, M in enumerate(MSB)])
+        # print(DATA)
 
-recv_serial_thread = Thread(target=recv_serial, args=["/dev/ttyACM0"])
+
+recv_serial_thread = Thread(target=recv_serial, args=["COM3"])
 recv_serial_thread.start()
-
 
 while True:
     if not len(DATA) == 0:
